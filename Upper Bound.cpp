cb71d55c3f13f3200eddb3e499a55c17ea4fbd06
Md. Parvej <62025097+Parvej23@users.noreply.github.com>
2022-08-16 01:23:49 +0600

    Add files via upload

---
+++ Upper Bound.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[]={1,2,3,3,3,4,5,6};
+int main()
+{
+    int l=0, r=8, tar=3;
+    int ans=10;
+    while(l<=r)
+    {
+        int mid=(l+r)>>1;
+        if(a[mid]>tar)
+        {
+            ans=min(ans, mid);
+            r=mid-1;
+        }
+        else l=mid+1;
+    }
+    cout<<ans<<endl;
+}
+
